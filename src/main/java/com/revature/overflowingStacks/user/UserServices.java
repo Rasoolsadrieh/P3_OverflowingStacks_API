5cb32a36b1655361e9185daf298fb0ed9b489a6f
JerryFaustin <jerfstn@gmail.com>
2022-06-23 11:45:27 -0400

    added create feature for user in services/servlet

--- src/main/java/com/revature/overflowingStacks/user/UserServices.java
+++ src/main/java/com/revature/overflowingStacks/user/UserServices.java
@@ -1,13 +1,23 @@
 package com.revature.overflowingStacks.user;
 
 import com.revature.overflowingStacks.util.interfaces.Serviceable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class UserServices implements Serviceable<User> {
+
+    private UserDao userDao;
+    @Autowired
+    public UserServices(UserDao userDao) {this.userDao = userDao;}
+
     @Override
-    public User create(User newObject) {
-        return null;
+    public User create(User newUser) {
+        return userDao.save(newUser);
     }
 
     @Override
--- src/main/java/com/revature/overflowingStacks/user/UserServlet.java
+++ src/main/java/com/revature/overflowingStacks/user/UserServlet.java
@@ -2,7 +2,11 @@ package com.revature.overflowingStacks.user;
 
 import com.revature.overflowingStacks.util.interfaces.Authable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +17,12 @@ public class UserServlet implements Authable {
     @Autowired
     public UserServlet(UserServices userServices){
         this.userServices=userServices;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userServices.create(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
 
