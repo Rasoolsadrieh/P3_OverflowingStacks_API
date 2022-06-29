bd011122a0672b4f8380dd807fe63543794a96cf
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-29 16:04:11 -0500

    fixing the cors issue

--- src/main/java/com/revature/overflowingStacks/user/UserServlet.java
+++ src/main/java/com/revature/overflowingStacks/user/UserServlet.java
@@ -54,7 +54,6 @@ public class UserServlet implements Authable {
     }
 
 
-    @CrossOrigin(origins = "https://overflowingstacks.azurewebsites.net", allowedHeaders = "*")
     @PostMapping("/register")
     public ResponseEntity<User> createUser(@RequestBody User user){
 
